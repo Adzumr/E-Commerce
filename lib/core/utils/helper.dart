50d21527398985e180ebaa1f8adb6a168eb1032c
Adzumr <adzumrjada@gmail.com>
2024-01-11 16:55:25 +0100

    Change INtro messages

--- lib/core/utils/helper.dart
+++ lib/core/utils/helper.dart
@@ -8,27 +8,21 @@ class Helper {
   static List<IntroductionMessage> introductionMessges = [
     IntroductionMessage(
       image: AppAssets().introImage,
-      title: "Welcome",
+      title: "Discover a New Way to Shop",
       subtitle:
-          "Welcome to ${AppConstants().appName}, your all-in-one utility services app!",
+          "Welcome to the future of online commerce! The Immersive Commerce App on Web 3.0 is your gateway to a revolutionary shopping experience. Discover a diverse range of products from our decentralized marketplace, where innovation meets convenience. Whether you're looking for the latest gadgets, trendy fashion, or unique handmade crafts, we have it all.",
     ),
     IntroductionMessage(
       image: AppAssets().introImage,
-      title: "Purchase Data and Airtime",
+      title: "Dive into Product Details Seamlessly",
       subtitle:
-          "Top up your phone with ease! Purchase data and airtime anytime, anywhere.",
+          "Immerse yourself in the world of each product with our intuitive navigation. Effortlessly browse through product listings and dive into comprehensive details by tapping on any item. From detailed descriptions and specifications to user reviews, every piece of information is at your fingertips. Experience a new level of engagement as you explore the products that match your style and preferences.",
     ),
     IntroductionMessage(
       image: AppAssets().introImage,
-      title: "Pay Bills",
+      title: "Personalize Your Shopping Journey",
       subtitle:
-          "Say goodbye to long queues! Pay your cable and electricity bills hassle-free.",
-    ),
-    IntroductionMessage(
-      image: AppAssets().introImage,
-      title: "Exam PINs",
-      subtitle:
-          "Need exam PINs? ${AppConstants().appName} has you covered. Easily purchase NECO, WEAC, and JAMB PINs.",
+          "Make the Immersive Commerce App truly yours! Create an account to unlock personalized features such as favoriting products with a simple tap. Stay effortlessly logged in across devices, ensuring a seamless shopping experience every time you open the app. Manage your profile to keep your information up-to-date, and let us enhance your shopping journey with tailored recommendations",
     ),
   ];
 }
